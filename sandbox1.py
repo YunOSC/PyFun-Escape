43dade28eebb0505ef850f78b2d0b5f863e27987
clooooode <jackey8616@gmail.com>
2018-09-14 23:38:45 +0800

    Beauty some code.lol

--- sandbox1.py
+++ sandbox1.py
@@ -1,17 +1,15 @@
 
-if __name__ == '__main__':
-    while True:
-        print('Please satisfied this python.')
-        print('def fun(____________):')
-        print('    c = a + b')
-        print('    return c')
-        print('print(func(10, 12) == 22)')
-        userInput = input('> ')
-        expr = '''
+
+expr = '''
 def func({0}):
     c = a + b
     return c
-print(func(10, 12) == 22)'''.format(userInput)
-        print(expr)
-        exec(expr)
+print(func(10, 12) == 22)'''
+
+if __name__ == '__main__':
+    while True:
+        print(expr.format('___________'))
+        userInput = input('> ')
+        print(expr.format(userInput))
+        exec(expr.format(userInput))
 
--- sandbox2.py
+++ sandbox2.py
@@ -1,20 +1,17 @@
 
+expr = '''
+def func({0}):
+    c = a + b
+    return c
+print(func(10, 12) == 22)'''
+
 if __name__ == '__main__':
     while True:
-        print('Please satisfied this python.')
-        print('def fun(____________):')
-        print('    c = a + b')
-        print('    return c')
-        print('print(func(10, 12) == 22)')
+        print(expr.format('___________'))
         userInput = input('> ')
         if '__import__' in userInput:
             print('Illegal use! (__import__)')
         else:
-            expr = '''
-def func({0}):
-    c = a + b
-    return c
-print(func(10, 12) == 22)'''.format(userInput)
-            print(expr)
-            exec(expr)
+            print(expr.format(userInput))
+            exec(expr.format(userInput))
 
